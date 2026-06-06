a1ddc78f7bdd60cb2117de5a78c8482295dab0f8
Neel Smith <dnsmith.neel@gmail.com>
2026-06-06 09:27:03 -0400

    Update data

--- hapsburg_census.py
+++ hapsburg_census.py
@@ -12,14 +12,54 @@ __generated_with = "0.23.9"
 app = marimo.App(width="columns")
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _():
     import marimo as mo
 
     return (mo,)
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""
+    ## 1793
+    """)
+    return
+
+
 @app.cell
+def _(pl, src1793):
+    df1793 = pl.read_csv(
+        src1793,
+        separator="|",
+        has_header=True,
+        infer_schema_length=1000,
+        truncate_ragged_lines=True
+    ).drop_nulls(subset=["District Number"])
+    return (df1793,)
+
+
+@app.cell(hide_code=True)
+def _(df1793):
+    df1793
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""
+    ## 1783
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(df1783):
+    df1783
+    return
+
+
+@app.cell(hide_code=True)
 def _():
     import polars as pl
     from pathlib import Path
@@ -27,19 +67,19 @@ def _():
     return Path, pl
 
 
-@app.cell
-def _(Path):
-    src1793 = (Path("1793-census") / "kolin-city.cex").resolve()
-    return (src1793,)
-
-
-@app.cell
+@app.cell(hide_code=True)
 def _(Path):
     src1783 = (Path("1783") / "Kolin-cols.cex").resolve()
     return (src1783,)
 
 
-@app.cell
+@app.cell(hide_code=True)
+def _(Path):
+    src1793 = (Path("1793-census") / "kolin-city.cex").resolve()
+    return (src1793,)
+
+
+@app.cell(hide_code=True)
 def _(pl, src1783):
     df1783 = pl.read_csv(
         src1783,
@@ -52,48 +92,7 @@ def _(pl, src1783):
 
 
 @app.cell
-def _(pl, src1793):
-    df1793 = pl.read_csv(
-        src1793,
-        separator="|",
-        has_header=True,
-        infer_schema_length=1000,
-        truncate_ragged_lines=True
-    ).drop_nulls(subset=["District Number"])
-
-    return (df1793,)
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md("""
-    ## 1793
-    """)
-    return
-
-
-@app.cell
-def _(df1793):
-    df1793
-    return
-
-
-@app.cell
 def _():
-    return
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md("""
-    ## 1783
-    """)
-    return
-
-
-@app.cell
-def _(df1783):
-    df1783
     return
 
 
